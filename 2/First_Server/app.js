ec3c50166d53cf3d7593e7404fb5c075700fa299
Christian <carl_hassel@hotmail.com>
2021-02-15 20:08:09 +0100

    Impæemented GET methods for mandatory

--- 2/First_Server/app.js
+++ 2/First_Server/app.js
@@ -12,4 +12,28 @@ app.get("/anotherpath/id", (req, res) => {
     res.send({ data: "some other things"});
 });
 
+const songs = [
+    {id: 1, title: 'A song'}, 
+    {id: 2, title: 'Two Princes'}, 
+    {id: 3, title: 'Jutlandia'}, 
+    {id: 4, title: 'Fade to Black'}
+];
+
+// implementation for GET /songs
+app.get("/songs", (req, res) => {
+    res.status(200).json({ songs: songs });
+});
+
+// implementation for GET /songs/id
+app.get('/songs/id/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const song = songs.find(song => song.id == id);
+    if (song != undefined) {
+        res.status(200).json({ song: song });
+    } else {
+        res.sendStatus(404);
+    }
+});
+
 app.listen(8080);
