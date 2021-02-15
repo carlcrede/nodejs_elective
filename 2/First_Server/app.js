217315d5d148a20f3fe3f7a585b00a67bdba9d85
Christian <carl_hassel@hotmail.com>
2021-02-15 20:12:51 +0100

    Cleanup

--- 2/First_Server/app.js
+++ 2/First_Server/app.js
@@ -4,14 +4,6 @@ const app = express();
 // consider what datatype to store your data in
 // you are allowed to hardcode the id
 
-app.get("/", (req, res) => {
-    res.send({ message: "First call to /"});
-});
-
-app.get("/anotherpath/id", (req, res) => {
-    res.send({ data: "some other things"});
-});
-
 const songs = [
     {id: 1, title: 'A song'}, 
     {id: 2, title: 'Two Princes'}, 
