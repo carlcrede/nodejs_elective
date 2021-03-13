5d530276193d1d2ff819f99d3deef2586570cf17
Christian <carl_hassel@hotmail.com>
2021-03-13 19:27:30 +0100

    Files from lecture 6

--- 2/4._Express_Wide_Web/app.js
+++ 2/4._Express_Wide_Web/app.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/welcome.html");
+});
+
+app.get("/pizza", (req, res) => {
+    res.sendFile(__dirname + "/public/pizza.html");
+});
+
+app.get("/whiskey", (req, res) => {
+    res.send({ barkeep: "Have a drink on the house!" })
+});
+let money = false;
+app.get("/pub", (req, res) => {
+    if (Number(req.query.money) > 500 ) {
+        res.redirect("/whiskey");
+    } else {
+        res.send({ doorman: "Sorry, you don't have any money" });
+    }
+   
 });
 
 app.listen(PORT, (error) => {
--- 2/Anime_Names_CRUD_API/app.js
+++ 2/Anime_Names_CRUD_API/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.json());
 
+let autoIncrement = 2;
+
 let animeNames = [
     {
         id: 1,
@@ -18,7 +20,7 @@ let animeNames = [
 
 // GET /anime_names
 app.get("/anime_names", (req, res) => {
-    res.send({ data: animeNames} );
+    res.send({ data: animeNames } );
 });
 
 // GET /anime_names/id
@@ -26,6 +28,30 @@ app.get("/anime_names/:id", (req, res) => {
     const id = Number(req.params.id);
     const animeName = animeNames.find(animeName => animeName.id === id);
     res.send({ data: animeName });
+});
+
+app.post("/anime_names", (req, res) => {
+    const newAnimeName = req.body;
+    newAnimeName.id = ++autoIncrement;
+    animeNames.push(newAnimeName);
+    res.send({ data: newAnimeName });
+});
+
+
+app.patch("/anime_names/:id", (req, res) => {
+    animeNames = animeNames.map(animeName => {
+        if (animeName.id === Number(req.params.id)) {
+            return {...animeName, ...req.body, id: animeName.id};
+        }
+        return animeName;
+    });
+    res.send({  });
+});
+
+app.delete("/anime_names/:id", (req, res) => {
+    const id = Number(req.params.id);
+    animeNames = animeNames.filter(animeName => animeName.id !== id);
+    res.sendStatus(204);
 });
 
 
