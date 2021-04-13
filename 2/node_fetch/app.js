93427de1f8084459c5fbcd7bb106c65248e17839
Christian <carl_hassel@hotmail.com>
2021-04-13 08:35:39 +0200

    Node-fetch homework added

--- 2/node_fetch/app.js
+++ 2/node_fetch/app.js
@@ -8,14 +8,14 @@ const google_url = 'https://www.google.com/';
 // using async-await
 app.get('/proxy', async (req, res) => {
     const response = await fetch(google_url);
-    const body = await response.text();
+    const body = await response.textConverted();  // using package 'encoding' to use textConverted for getting the correct encoding
     res.send(body);
 });
 
 // using promises
 /* app.get('/proxy', (req, res) => {
     fetch('https://www.google.com/')
-        .then(response => response.text())
+        .then(response => response.textConverted())
         .then(html => res.send(html))
         .catch(err => console.log('Something went wrong', err))
 }); */
