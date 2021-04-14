874f881d10cac3e11b46d39f4a293388236a2b79
Christian <carl_hassel@hotmail.com>
2021-04-14 16:31:58 +0200

    Added remaining pages -- still need to add actual content -- and something interesting for frontpage..

--- 2/Nodefolio/app.js
+++ 2/Nodefolio/app.js
@@ -9,18 +9,29 @@ app.use(express.json());
 // server-side rendering
 // read file as text
 // send text content to the client
-const header = fs.readFileSync(__dirname + '/public/header/header.html', 'utf-8');
-const frontpage = fs.readFileSync(__dirname + '/public/frontpage/frontpage.html', 'utf-8');
-const footer = fs.readFileSync(__dirname + '/public/footer/footer.html', 'utf-8');
-const projects_page = fs.readFileSync(__dirname + '/public/projects/projects.html', 'utf-8');
+const header = fs.readFileSync(__dirname + '/public/header/header.html', 'utf-8'),
+    frontpage = fs.readFileSync(__dirname + '/public/frontpage/frontpage.html', 'utf-8'),
+    footer = fs.readFileSync(__dirname + '/public/footer/footer.html', 'utf-8'),
+    projects = fs.readFileSync(__dirname + '/public/projects/projects.html', 'utf-8'),
+    skills = fs.readFileSync(__dirname + '/public/skills/skills.html', 'utf-8'),
+    contact = fs.readFileSync(__dirname + '/public/contact/contact.html', 'utf-8');
 
 app.get('/', (req, res) => {
     res.send(header + frontpage + footer);
 });
 
-app.get('/projects', (req, res) => {
-    res.send(header + projects_page + footer);
+app.get('/skills', (req, res) => {
+    res.send(header + skills + footer);
 });
+
+app.get('/projects', (req, res) => {
+    res.send(header + projects + footer);
+});
+
+app.get('/contact', (req, res) => {
+    res.send(header + contact + footer);
+});
+
 
 
 const server = app.listen(port, (error) => {
--- 2/Nodefolio/public/frontpage/frontpage.js
+++ 2/Nodefolio/public/frontpage/frontpage.js
@@ -1,2 +1,16 @@
 let copyright_element = document.getElementById('footer-copyright');
 copyright_element.innerHTML = `Copyright <i class="mdi mdi-copyright"></i> ${new Date().getFullYear()}`;
+
+$(() => {
+    typeintro();
+});
+
+const intro_text = 'Hi there. My name is Christian and I am a developer. Prepare for your mind to be blown.';
+const intro_speed = 90;
+let i = 0;
+
+function typeintro() {
+    $('#intro').append(intro_text.charAt(i));
+    i++;
+    setTimeout(typeintro, intro_speed+i);
+}
